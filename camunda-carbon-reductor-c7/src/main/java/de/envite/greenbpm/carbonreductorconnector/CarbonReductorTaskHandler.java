1da9a6f5df77edb66d717c2edf812b7238895ba3
Luc Weinbrecht <luc@weinbrecht.de>
2023-02-10 13:25:53 +0100

    started building c7 worker

---
+++ camunda-carbon-reductor-c7/src/main/java/de/envite/greenbpm/carbonreductorconnector/CarbonReductorTaskHandler.java
@@ -0,0 +1,80 @@
+package de.envite.greenbpm.carbonreductorconnector;
+
+
+import de.envite.greenbpm.carbonreductor.core.domain.model.CarbonReduction;
+import de.envite.greenbpm.carbonreductor.core.domain.model.CarbonReductorConfiguration;
+import de.envite.greenbpm.carbonreductor.core.domain.service.CarbonReductorException;
+import de.envite.greenbpm.carbonreductor.core.usecase.in.DelayCalculator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Map;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+@ExternalTaskSubscription(topicName = "CarbonReductor")
+public class CarbonReductorTaskHandler implements ExternalTaskHandler {
+
+    public static final int RETRIES_MAGIC_NUMBER = 999;
+
+    private final DelayCalculator delayCalculator;
+    private final CarbonReductorVariableMapper carbonReductorVariableMapper;
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        log.debug("Entering Carbon Reductor with variables: {}", externalTask.getAllVariables());
+
+        if (maxRetriesReached(externalTask)) {
+            log.info("Completing previously time shifted job {}", externalTask.getProcessInstanceId());
+            externalTaskService.complete(externalTask);
+            return;
+        }
+
+        CarbonReductorConfiguration carbonReductorConfiguration = carbonReductorVariableMapper.mapToDomain(externalTask.getAllVariables());
+
+        try {
+            CarbonReduction carbonReductorOutput = delayCalculator.calculateDelay(carbonReductorConfiguration);
+            delayOrExecute(externalTask, externalTaskService, carbonReductorOutput);
+        } catch (CarbonReductorException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void delayOrExecute(ExternalTask externalTask, ExternalTaskService externalTaskService, CarbonReduction carbonReductorOutput) {
+        Map<String, Object> outputVariables = carbonReductorVariableMapper.mapFromDomain(carbonReductorOutput);
+
+        if (carbonReductorOutput.getDelay().isExecutionDelayed()) {
+            Duration duration = Duration.ofMillis(carbonReductorOutput.getDelay().getDelayedBy());
+            log.info("Time shifting job {} by {}", externalTask.getProcessInstanceId(), duration);
+            failJobWithRetry(externalTaskService, externalTask, duration, outputVariables);
+        } else {
+            log.info("Executing job {} immediately", externalTask.getProcessInstanceId());
+            externalTaskService.complete(externalTask, outputVariables);
+        }
+    }
+
+    private void failJobWithRetry(ExternalTaskService externalTaskService, ExternalTask externalTask,
+                                  Duration duration, Map<String, Object> variableMap) {
+        externalTaskService.handleFailure(
+                externalTask.getId(),
+                "Time shifting execution",
+                "Time shifting execution due for a lower carbon footprint",
+                RETRIES_MAGIC_NUMBER,
+                duration.get(MILLIS),
+                variableMap,
+                null);
+    }
+
+    private boolean maxRetriesReached(ExternalTask externalTask) {
+        return externalTask.getRetries() != null && externalTask.getRetries() == RETRIES_MAGIC_NUMBER;
+    }
+}
---
+++ camunda-carbon-reductor-c7/src/main/java/de/envite/greenbpm/carbonreductorconnector/CarbonReductorVariableMapper.java
@@ -0,0 +1,44 @@
+package de.envite.greenbpm.carbonreductorconnector;
+
+import de.envite.greenbpm.carbonreductor.core.domain.model.CarbonReduction;
+import de.envite.greenbpm.carbonreductor.core.domain.model.CarbonReductorConfiguration;
+import de.envite.greenbpm.carbonreductor.core.domain.model.input.Milestone;
+import de.envite.greenbpm.carbonreductor.core.domain.model.input.Timeshift;
+import de.envite.greenbpm.carbonreductor.core.domain.model.input.carbonreductormode.CarbonReductorMode;
+import de.envite.greenbpm.carbonreductor.core.domain.model.input.location.Location;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+class CarbonReductorVariableMapper {
+    public CarbonReductorConfiguration mapToDomain(Map<String, Object> allVariables) {
+        return new CarbonReductorConfiguration(
+                new Location((String) allVariables.get("location")),
+                new CarbonReductorMode((String) allVariables.get("carbonReductorMode")),
+                new Milestone((String) allVariables.get("milestone")),
+                null,
+                mapIfNotNull((String) allVariables.get("maximumProcessDuration")),
+                null
+        );
+    }
+
+    private Timeshift mapIfNotNull(String input) {
+        if (input == null) {
+            return null;
+        }
+        return new Timeshift(input);
+    }
+
+    public Map<String, Object> mapFromDomain(CarbonReduction output) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("executionDelayed", output.getDelay().isExecutionDelayed());
+        variables.put("originalCarbon", output.getOriginalCarbon().getValue());
+        variables.put("actualCarbon", output.getActualCarbon().getValue());
+        variables.put("savedCarbon", output.getSavedCarbon().getValue());
+        variables.put("carbonReduction", output.calculateReduction().getValue());
+        variables.put("delayedBy", output.getDelay().getDelayedBy());
+        return variables;
+    }
+}
