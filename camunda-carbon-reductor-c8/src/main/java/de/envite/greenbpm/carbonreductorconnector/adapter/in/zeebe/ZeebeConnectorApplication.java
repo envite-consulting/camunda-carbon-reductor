ca0a98a55b663efba42e995bd3175bdba9f8c1bc
Luc Weinbrecht <luc@weinbrecht.de>
2023-07-26 07:27:19 +0200

    updated zeebe to 8.2.3. removed zeebe bom. updated connector core version to 0.11.3. updated spring boot to 2.7.14

--- camunda-carbon-reductor-c8/src/main/java/de/envite/greenbpm/carbonreductorconnector/adapter/in/zeebe/ZeebeConnectorApplication.java
+++ camunda-carbon-reductor-c8/src/main/java/de/envite/greenbpm/carbonreductorconnector/adapter/in/zeebe/ZeebeConnectorApplication.java
@@ -1,12 +1,10 @@
 package de.envite.greenbpm.carbonreductorconnector.adapter.in.zeebe;
 
 import de.envite.greenbpm.carbonreductor.core.EnableCarbonReductorCore;
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 @EnableCarbonReductorCore
 public class ZeebeConnectorApplication {
 
