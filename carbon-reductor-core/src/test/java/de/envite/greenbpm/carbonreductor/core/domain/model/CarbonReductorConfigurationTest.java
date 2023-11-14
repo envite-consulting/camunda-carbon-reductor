092830e544b8c05ed798978dc2d9772831bb51f8
Luc Weinbrecht <luc@weinbrecht.de>
2023-11-14 19:03:01 +0100

    updated to spring boot 3.1.5. updated all version (if available). added missing jaxb-api dependencies for C7. close #88
    Co-authored-by: Maximilian Behrendt maximilian.behrendt@envite.de

--- carbon-reductor-core/src/test/java/de/envite/greenbpm/carbonreductor/core/domain/model/CarbonReductorConfigurationTest.java
+++ carbon-reductor-core/src/test/java/de/envite/greenbpm/carbonreductor/core/domain/model/CarbonReductorConfigurationTest.java
@@ -21,7 +21,7 @@ class CarbonReductorConfigurationTest {
     void should_set_default_for_error_handling() {
         CarbonReductorConfiguration config = new CarbonReductorConfiguration(
                 Locations.SWEDEN_CENTRAL.asLocation(),
-                new Milestone(OffsetDateTime.now(ZoneOffset.UTC)),
+                new Milestone(OffsetDateTime.now(ZoneOffset.UTC).minusHours(1)),
                 new Timeshift(String.valueOf(Duration.ofHours(3))),
                 new Timeshift(String.valueOf(Duration.ofHours(12))),
                 null,
@@ -35,7 +35,7 @@ class CarbonReductorConfigurationTest {
     void should_set_not_default_for_error_handling_if_value_is_present() {
         CarbonReductorConfiguration config = new CarbonReductorConfiguration(
                 Locations.SWEDEN_CENTRAL.asLocation(),
-                new Milestone(OffsetDateTime.now(ZoneOffset.UTC)),
+                new Milestone(OffsetDateTime.now(ZoneOffset.UTC).minusHours(1)),
                 new Timeshift(String.valueOf(Duration.ofHours(3))),
                 new Timeshift(String.valueOf(Duration.ofHours(12))),
                 null,
@@ -50,7 +50,7 @@ class CarbonReductorConfigurationTest {
 
         private final Location location = Locations.SWEDEN_CENTRAL.asLocation();
 
-        private final Milestone milestone = new Milestone(OffsetDateTime.now(ZoneOffset.UTC));
+        private final Milestone milestone = new Milestone(OffsetDateTime.now(ZoneOffset.UTC).minusHours(1));
         private final Timeshift timeshift = new Timeshift(String.valueOf(Duration.ofHours(3)));
 
         @Test
